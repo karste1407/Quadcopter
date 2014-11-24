111e77061b3aeabe8499c6ef7c254f1dfc06affe
Northo <thorvald.tb@gmail.com>
2014-11-24 18:32:34 +0100

    kommentarer

--- Kode/Test/fjernkontroll/remoteToEar/remoteToEar.ino
+++ Kode/Test/fjernkontroll/remoteToEar/remoteToEar.ino
@@ -1,4 +1,4 @@
-const int rxPin = 8;
+const int rxPin = 8; //'const' forteller compileren(det programmet som oversetter fra engelsk til datakode(0'er og 1'ere) at denne variabelen ikke kan forandres(hvis man senere i programmet prøver å gi den en ny verdi vil man få en error). Det er strengt tatt ikke nødvendig, men er mer elegant, og hvis hjelpe å ungå feil i store programmer
 const int sound = 13;
 
 void setup() {
