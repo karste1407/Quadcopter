6c3319142a4b68683ed5947820e3474fde4668ef
Thorvald <none>
2015-02-04 14:30:55 +0100

    Made it array-based

--- Kode/ESC/FirstTestRemoteControl.c
+++ Kode/ESC/FirstTestRemoteControl.c
@@ -3,23 +3,19 @@
 int value = 0, remoteValue;
 #define REMOTE_PIN = 8;
 
-Servo firstESC, secondESC, thirdESC, fourthESC;
+Servo motors[4];
+int motorPins[] = {9, 10, 11, 3}; //Must have PWM support, nut sure about pin 3
 
 void setup() {
-  firstESC.attach(9);
-  secondESC.attach(10);
-  thirdESC.attach(11);
-  //I'm not sure 12 has PWM support, just wrote this as an example at home
-  fourthESC.attach(12);
+  for(int i = 0; i < 4; i++)
+    motors[i].attach(motorPins[i]);
 
   pinMode(REMOTE_PIN, INPUT);
 }
 
 void loop() {
-  firstESC.writeMicroseconds(value);
-  secondESC.writeMicroseconds(value);
-  thirdESC.writeMicroseconds(value);
-  fourthESC.writeMicroseconds(value);
+  for(int i = 0; i < 4; i++)
+    motors[i].write(value);
 
   //pulseIn returns an integer that gives the number of microseconds of a pulse, eg. ______/¯\_____/¯¯¯\____ The 'HIGH' means that we are interested in the hight voltage pulses, not drops.
   value = pulseIn(REMOTE_PIN, HIGH);
